8b8267fbd30da9320e27f8fe23497f8f27c051da
muthukarka <muthukarka@gmail.com>
2026-01-19 14:43:32 +0530

    Update EasypgmTry.sql

--- EasypgmTry.sql
+++ EasypgmTry.sql
@@ -14,3 +14,19 @@ having salrank = 3
 order by spend desc)
 select user_id,spend,transaction_date, salrank
 from rann;
+
+
+select user_id,spend,transaction_date,
+row_number() over(partition by user_id order by transaction_date) as salrank
+from transactions
+having salrank = 3
+order by spend desc;
+
+with rann as(
+select user_id,spend,transaction_date,
+row_number() over(partition by transaction_date order by spend) as salrank
+from transactions
+having salrank = 3
+order by spend desc)
+select user_id,spend,transaction_date, salrank
+from rann;
